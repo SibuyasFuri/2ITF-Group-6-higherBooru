5b464a3e792286ee86918d618d65de508a90a1d3
Andrie Macatangay <andriepaul.macatangay.cics@ust.edu.ph>
2023-05-23 13:20:42 +0800

    Complete 100%

--- imageboard_db.sql
+++ imageboard_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2023 at 03:53 PM
+-- Generation Time: May 23, 2023 at 07:20 AM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,15 +41,9 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id`, `name`, `image_url`, `tags`, `dateUploaded`, `user_id`) VALUES
-(61, 'swag', 'IMG-646b083f7a60a7.13053271.png', 'swag,', '2023-05-22', 132),
-(62, '3', 'IMG-646b0878c512a1.00342183.png', '3,', '2023-05-22', 999),
-(63, '4', 'IMG-646b096edbfde1.26123076.png', ',', '2023-05-22', 635),
-(64, '5', 'IMG-646b098ac8a243.55387716.png', ',', '2023-05-22', 252),
-(65, '123', 'IMG-646b0f3bd14503.97459502.png', ',', '2023-05-22', 224),
-(66, '123', 'IMG-646b125eb002e3.12665629.png', '1231,', '2023-05-22', 645),
-(67, '123', 'IMG-646b154b1c90f0.77625495.png', '123,', '2023-05-22', 997),
-(68, '1112312', 'IMG-646b15770a9637.10608633.png', ',', '2023-05-22', 970),
-(69, '123', 'IMG-646b17ebc99271.71083140.png', '1,', '2023-05-22', 915);
+(95, 'Lucy in Space', 'IMG-646c460e209b30.89369509.png', 'Lucy Space Cyberpunk Edgerunners Cyberpunk_Edgerunners Moon,', '2023-05-23', 787),
+(96, 'Lucy', 'IMG-646c46764143d1.93744725.jpg', 'Lucy Chill Smoking,', '2023-05-23', 23),
+(97, 'Cat', 'IMG-646c46a7099076.03867783.jpeg', 'Cat Cute Smile Kawaii,', '2023-05-23', 787);
 
 -- --------------------------------------------------------
 
@@ -70,15 +64,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_id`, `user_name`, `password`, `date`) VALUES
-(63, 132, '1', '1', '2023-05-22 06:14:14'),
-(64, 999, '3', '3', '2023-05-22 06:15:10'),
-(65, 635, '4', '4', '2023-05-22 06:19:19'),
-(66, 252, '5', '5', '2023-05-22 06:19:46'),
-(67, 224, '123', '123', '2023-05-22 06:44:03'),
-(68, 645, 'test123', 'test123', '2023-05-22 06:57:24'),
-(69, 997, '67', '67', '2023-05-22 07:09:33'),
-(70, 970, 'abcv', 'ab', '2023-05-22 07:10:37'),
-(71, 915, 'zzzz', '12', '2023-05-22 07:21:02');
+(95, 787, 'Ada', 'ada123', '2023-05-23 04:49:35'),
+(96, 23, 'Gian', 'gian123', '2023-05-23 04:51:34');
 
 --
 -- Indexes for dumped tables
@@ -104,13 +91,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
